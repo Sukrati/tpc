2bf645986bed7cf2e364a96419014c434d76d5e9
Meetasha29 <meetaasha29@gmail.com>
2017-04-12 15:50:48 +0530

    added the database

--- oopData.sql
+++ oopData.sql
@@ -1,57 +1,56 @@
 Create table Student(
-  sid int(3) primary key,
+  sid varchar(10) primary key,
   sname varchar(20) not null,
   spassword varchar(10) not null,
   sEmailId varchar(15),
-  cpi decimal(2,2) not null,
+  cpi decimal(4,2) not null,
   placementStatus bit(1) not null,
   placedCompany varchar(20),
-  salary decimal(3,2) not null
+  salary decimal(3,2)
 );
 
 Create table TechStudent(
-  sid int(3) primary key,
-  branch varchar(10) not null,
-  requirements varchar(20)
+  sid varchar(10) primary key,
+  branch varchar(10) not null
   Constraint fk_sidt Foreign key(sid) references Student(sid)
 );
 
 Create table NonTechStudent(
-  sid int(3) primary key,
-  requirements varchar(20)
+  sid varchar(10) primary key,
+  interest varchar(20)
   Constraint fk_sidn Foreign key(sid) references Student(sid)
 );
 
 Create table Company(
-  cid int(3) primary key,
+  cIN int(3) primary key,
   cname varchar(20) not null,
-  cpassword varchar(10) not null,
   cEmailId varchar(15),
-  cpiCriteria decimal(2,2) not null,
+  cpiCriteria decimal(4,2) not null,
   city varchar(10),
-  StudentsRequired int(3) not null,
+  visitDate date,
   salary decimal(3,2) not null
 );
 
 Create table TechCompany(
-  cid int(3) primary key,
+  cIN int(3) primary key,
   branch varchar(10) not null,
   requirements varchar(20)
-  Constraint fk_cidt Foreign key(cid) references Company(cid)
+  Constraint fk_cidt Foreign key(cIN) references Company(cIN)
 );
 
 Create table NonTechCompany(
-  sid int(3) primary key,
+  cIN int(3) primary key,
   requirements varchar(20)
-  Constraint fk_cidn Foreign key(cid) references Company(cid)
+  Constraint fk_cidn Foreign key(cIN) references Company(cIN)
 );
 
 Create table CellMember(
-  clid int(3) primary key,
-  clname varchar(20) not null,
-  clpassword varchar(10) not null,
-  clEmailId varchar(15),
+  cid int(3) primary key,
+  cname varchar(20) not null,
+  cpassword varchar(10) not null,
+  cEmailId varchar(15),
   ContactNumber int(10) not null unique,
   year char(3),
+  gender char(1),
   category varchar(20)
 );
