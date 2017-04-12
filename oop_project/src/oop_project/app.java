d017196d6fcde1647ed166504af0d1d5cb412f3d
Meetasha29 <meetaasha29@gmail.com>
2017-04-12 16:50:10 +0530

    added oopProject

---
+++ oop_project/src/oop_project/app.java
@@ -0,0 +1,16 @@
+package oop_project;
+
+public class app {
+
+	public static void main(String[] args) {
+		//NonTechstud s1=new NonTechstud();
+		//s1=NonTechstud.showDetails(1);
+		TechStudent s2=new TechStudent();
+		//s2.getDetails("1");
+		//System.out.println(s2.id);
+		tpo t=new tpo();
+		//t.insertTechStud("4", "meetasga","dd", "dk@hmh", 10.33, 1,"abcd", 2.43, "cse");
+		t.editTechStudent("sid", "2", "15ucs072");
+	}
+
+}
