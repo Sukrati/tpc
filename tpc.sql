122299114e6ef03168b88865edbfc0c19f681c34
Meetasha29 <meetaasha29@gmail.com>
2017-04-14 12:55:34 +0530

    changed the database

--- tpc.sql
+++ tpc.sql
@@ -6,11 +6,12 @@ Create table Student(
   cpi decimal(4,2) not null,
   placementStatus bit(1) not null,
   placedCompany varchar(20),
-  salary decimal(3,2)
+  salary decimal(4,2)
 );
 
 Create table TechStudent(
   sid varchar(10) primary key,
+  interest varchar(20),
   branch varchar(10) not null,
   Constraint fk_sidt Foreign key(sid) references Student(sid)
 );
@@ -22,34 +23,34 @@ Create table NonTechStudent(
 );
 
 Create table Company(
-  cIN int(3) primary key,
+  cIN int primary key,
   cname varchar(20) not null,
-  cEmailId varchar(15),
+  cEmailId varchar(25),
   cpiCriteria decimal(4,2) not null,
   city varchar(10),
   visitDate date,
-  salary decimal(3,2) not null
+  salary decimal(4,2) not null
 );
 
 Create table TechCompany(
-  cIN int(3) primary key,
+  cIN int primary key,
   branch varchar(10) not null,
   requirements varchar(20),
   Constraint fk_cidt Foreign key(cIN) references Company(cIN)
 );
 
 Create table NonTechCompany(
-  cIN int(3) primary key,
+  cIN int primary key,
   requirements varchar(20),
   Constraint fk_cidn Foreign key(cIN) references Company(cIN)
 );
 
 Create table CellMember(
-  cid int(3) primary key,
+  cid varchar(10) primary key,
   cname varchar(20) not null,
   cpassword varchar(10) not null,
-  cEmailId varchar(15),
-  ContactNumber int(10) not null unique,
+  cEmailId varchar(30);
+  ContactNumber varchar(15) unique,
   year char(3),
   gender char(1),
   category varchar(20)
